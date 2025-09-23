d0d87de6dbd2569bf66c67058a92fd2d74bf32a4
Paul Laisné <paul.laisne2004@gmail.com>
2025-09-23 11:14:27 +0200

    data_exploration update

--- data_exploration.R
+++ data_exploration.R
@@ -1,6 +1,16 @@
 # Load the CSV file into a data frame and choosing how many rows and columns
 
-data <- read.csv("meteo.csv", header = TRUE, sep = ";", )
+data <- read.csv("meteo.csv", header = TRUE, sep = ";", nrows = 100)
 
 # Display the first few rows of the data frame
 head(data[, 1:5])  # Display first 5 columns for brevity
+
+# Display the structure of the data frame to understand its contents
+str(data) 
+
+# Summary statistics of the data frame
+summary(data)
+
+# Visualize the distribution of a specific variable, e.g., "Pression au niveau de la mer" and save the plot as a PNG file
+png("pression_boxplot.png", width = 1200, height = 1500)
+boxplot(data$Pression.au.niveau.mer, main = "Boxplot of Pression au niveau de la mer", ylab = "Pression (hPa)")
