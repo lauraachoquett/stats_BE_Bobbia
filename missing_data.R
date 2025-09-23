0315fc39dde383de83e8ab1f31153ef68c21df4b
Paul Laisné <paul.laisne2004@gmail.com>
2025-09-23 13:23:20 +0200

    missing_data drop

--- missing_data.R
+++ missing_data.R
@@ -7,3 +7,12 @@ print(dim(data))  # Print the dimensions of the data frame
 missing_values <- colSums(is.na(data))
 cat("Total missing values in the dataset per column:\n")
 print(missing_values[missing_values > 0])
+
+# Drop the columns if there is more than 30% of missing values
+threshold <-  0.3 * nrow(data)
+cols_to_drop <- names(missing_values[missing_values > threshold])
+data_cleaned <- data[, !(names(data) %in% cols_to_drop)]
+cat("Dropped columns with more than 30% missing values:\n")
+
+
+write.csv(data_cleaned, "observations_cleaned.csv", row.names = FALSE)
