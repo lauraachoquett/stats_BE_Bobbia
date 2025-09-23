425ed49e25aa9108ee07017e15e58c6380d10dd5
Paul Laisné <paul.laisne2004@gmail.com>
2025-09-23 11:50:34 +0200

    missing values

--- data_exploration.R
+++ data_exploration.R
@@ -1,16 +1,22 @@
 # Load the CSV file into a data frame and choosing how many rows and columns
 
-data <- read.csv("meteo.csv", header = TRUE, sep = ";", nrows = 100)
+data <- read.csv("meteo.csv", header = TRUE, sep = ";")
+print(dim(data))  # Print the dimensions of the data frame
 
-# Display the first few rows of the data frame
-head(data[, 1:5])  # Display first 5 columns for brevity
+# # Display the first few rows of the data frame
+# head(data[, 1:5])  # Display first 5 columns for brevity
 
-# Display the structure of the data frame to understand its contents
-str(data) 
+# # Display the structure of the data frame to understand its contents
+# str(data) 
 
-# Summary statistics of the data frame
-summary(data)
+# # Summary statistics of the data frame
+# summary(data)
 
-# Visualize the distribution of a specific variable, e.g., "Pression au niveau de la mer" and save the plot as a PNG file
-png("pression_boxplot.png", width = 1200, height = 1500)
-boxplot(data$Pression.au.niveau.mer, main = "Boxplot of Pression au niveau de la mer", ylab = "Pression (hPa)")
+# # Visualize the distribution of a specific variable, e.g., "Pression au niveau de la mer" and save the plot as a PNG file
+# png("pression_boxplot.png", width = 1200, height = 1500)
+# boxplot(data$Pression.au.niveau.mer, main = "Boxplot of Pression au niveau de la mer", ylab = "Pression (hPa)")
+
+# Check for missing values in the dataset per column and get a the missing values per column
+missing_values <- colSums(is.na(data))
+cat("Total missing values in the dataset per column:\n")
+print(missing_values[missing_values > 0])
